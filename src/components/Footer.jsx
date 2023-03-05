06258a6818049630d4ccd482b23467a8e8fb05a2
efmorales <54375116+efmorales@users.noreply.github.com>
2023-03-05 16:47:20 -0500

    images can be seen in production now

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,27 @@
+import githubIcon from "./gh-s.png"
+import instaIcon from "./insta-s.png"
+import twIcon from "./tw-s.png"
+
 export default function Footer () {
     return (
         <div className="footer">
 
             <a href="https://twitter.com/efmoral">
                 <button className="footer-tw">
-                    <img src="src\assets\tw-s.png" alt="twitter-link" />
+                    <img src={twIcon} alt="twitter-link" />
                 </button>
             </a>
 
             <a href="https://www.instagram.com/zanalphabetic/">
                 <button className="footer-insta">
-                    <img src="src\assets\insta-s.png" alt="instagram-link" />
+                    <img src={instaIcon} alt="instagram-link" />
                 </button>
             </a>
 
 
             <a href="https://github.com/efmorales">
                 <button className="footer-gh">
-                    <img src="src\assets\gh-s.png" alt="github-link" />
+                    <img src={githubIcon} alt="github-link" />
                 </button>
             </a>
 
