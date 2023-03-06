a0f94740e2fb4e76904cbdf9282aa1c6367d2f19
efmorales <54375116+efmorales@users.noreply.github.com>
2023-03-05 22:48:17 -0500

    replaced mockup content with actual content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ export default function About() {
     return (
         <div className="about">
             <h3 className="about--title">About</h3>
-            <p className="about--content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi hic non maxime aliquid, possimus voluptatibus, rerum temporibus autem nemo, ullam laboriosam rem? Sit quod eveniet doloribus ipsum nostrum saepe sequi.</p>
+            <p className="about--content"> Bachelor in Anthropology. AoS in Computer Science and Web Architecture student. 4 years of experience working as a Design researcher developing web apps. Last specialization was a year-long Professional Certificate in Digital Transformation from MIT.</p>
         </div>
     )
 }
--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -2,7 +2,7 @@ export default function Interests() {
     return (
         <div className="interests">
             <h3 className="interests--title">Interests</h3>
-            <p className="interests--content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi hic non maxime aliquid, possimus voluptatibus, rerum temporibus autem nemo, ullam laboriosam rem? Sit quod eveniet doloribus ipsum nostrum saepe sequi.</p>
+            <p className="interests--content">I love digital painting, dancing and videogames (Celeste, Overwatch, Hades). I like challenges because it's the best way to learn. </p>
         </div>
     )
 }
