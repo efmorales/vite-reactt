82892b9f81c32c092329c482ea9a9cb2bb571220
efmorales <54375116+efmorales@users.noreply.github.com>
2023-03-05 23:00:49 -0500

    fixed typo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ export default function About() {
     return (
         <div className="about">
             <h3 className="about--title">About</h3>
-            <p className="about--content"> Bachelor in Anthropology. AoS in Computer Science and Web Architecture student. 4 years of experience working as a Design researcher planning, prototyping and developing user-centered web apps. My last specialization was a year-long Professional Certificate in Digital Transformation from MIT.</p>
+            <p className="about--content"> From Lima, Peru; currently living in Brooklyn, New York. Bachelor in Anthropology. AoS in Computer Science and Web Architecture student. 4 years of experience working as a Design researcher planning, prototyping and developing user-centered web apps. My last specialization was a year-long Professional Certificate in Digital Transformation from MIT.</p>
         </div>
     )
 }
